dfb660e222b131480c3d3fb6213a75917a69be79
Cassandra Loh <cassandra@Cassandras-MacBook-Air.local>
2018-04-11 18:03:17 -1000

    completed ex7

--- exercises.js
+++ exercises.js
@@ -69,7 +69,6 @@ Declare a variable named `stringOfNumbers` and assign its value to an empty stri
 Write a for-loop that concatenates a Number value into that string on each iteration, starting at `10` and continuing up to and including `20`. Console.log your result. It should read "1011121314151617181920"*/
 
 var stringOfNumbers = " ";
-console.log (stringOfNumbers)
 
 for (var i = 10; i < 21; i++){
   stringOfNumbers += i
@@ -91,13 +90,23 @@ for (var i = 0; i < 101; i++){
 }
 console.log (evenNumArr)
 
+
 /* 6) Up the Odds
 Declare a variable named `oddSum` and assign it to the Number value 0.
 
 Write a for-loop that will sum up odd numbers to the `oddSum` variable. We want to add 50 odd numbers starting from 1.
 Console.log your results.
 */
-  
+
+var oddSum = 0
+
+for (var i = 1; i  < 100; i++){
+  if (i%2 === 1){
+    oddSum += i;
+  }
+}
+console.log (oddSum);
+
 
 /* 7) Oops There It is
 Declare a variable named `oopsArray` and assign its to the following array: `[ 'turn' , , 'down' , , 'for' , , 'what' ]`.
@@ -105,7 +114,13 @@ Note that every odd index value in `oopsArray` is currently `undefined`. Using a
 
 [ 'turn' , 'nope' , 'down' , 'nope' , 'for' , 'nope' , 'what' ]
 */
+var oopsArray = ["turn", , "down", , "for", , "what"]
 
+for (var i = 0; i < oopsArray.length; i++){
+  if (i%2 === 1)
+  oopsArray[i] = "nope"
+}
+console.log (oopsArray)
 
 /* 8) Is It There Oops
 Using a for-loop, iterate through the Array stored at `oopsArray` backwards. Console.log your result. It should look like this:
